e750d933a620d4ea147c5936b6665152941a4828
Stephen Williams <swilliams@locusrobotics.com>
2018-07-02 22:40:53 -0700

    Added some basic unit tests

---
+++ fuse_core/test/test_constraint.cpp
@@ -0,0 +1,116 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <fuse_core/constraint.h>
+#include <fuse_core/macros.h>
+#include <fuse_core/uuid.h>
+
+#include <gtest/gtest.h>
+
+#include <initializer_list>
+#include <vector>
+
+
+/**
+ * @brief Dummy constraint implementation for testing
+ */
+class TestConstraint : public fuse_core::Constraint
+{
+public:
+  SMART_PTR_DEFINITIONS(TestConstraint);
+
+  TestConstraint(std::initializer_list<fuse_core::UUID> variable_uuid_list) :
+    fuse_core::Constraint(variable_uuid_list)
+  {
+  }
+
+  template<typename VariableUuidIterator>
+  TestConstraint(VariableUuidIterator first, VariableUuidIterator last) :
+    fuse_core::Constraint(first, last)
+  {
+  }
+
+  void print(std::ostream& stream = std::cout) const override {}
+  ceres::CostFunction* costFunction() const override { return nullptr; }
+  fuse_core::Constraint::UniquePtr clone() const override { return TestConstraint::make_unique(*this); }
+};
+
+
+TEST(Constraint, Constructor)
+{
+  // Create a constraint with a single UUID
+  {
+    fuse_core::UUID variable_uuid1 = fuse_core::uuid::generate();
+    TestConstraint constraint{variable_uuid1};
+    ASSERT_EQ(1, constraint.variables().size());
+    ASSERT_EQ(variable_uuid1, constraint.variables().at(0));
+  }
+  // Create a constraint with multiple UUIDs
+  {
+    fuse_core::UUID variable_uuid1 = fuse_core::uuid::generate();
+    fuse_core::UUID variable_uuid2 = fuse_core::uuid::generate();
+    fuse_core::UUID variable_uuid3 = fuse_core::uuid::generate();
+    TestConstraint constraint{variable_uuid1, variable_uuid2, variable_uuid3};
+    ASSERT_EQ(3, constraint.variables().size());
+    ASSERT_EQ(variable_uuid1, constraint.variables().at(0));
+    ASSERT_EQ(variable_uuid2, constraint.variables().at(1));
+    ASSERT_EQ(variable_uuid3, constraint.variables().at(2));
+  }
+  // Create a constraint using the iterator constructor
+  {
+    std::vector<fuse_core::UUID> variable_uuids;
+    variable_uuids.push_back(fuse_core::uuid::generate());
+    variable_uuids.push_back(fuse_core::uuid::generate());
+    variable_uuids.push_back(fuse_core::uuid::generate());
+    variable_uuids.push_back(fuse_core::uuid::generate());
+    TestConstraint constraint(variable_uuids.begin(), variable_uuids.end());
+    ASSERT_EQ(variable_uuids.size(), constraint.variables().size());
+    for (size_t i = 0; i < variable_uuids.size(); ++i)
+    {
+      ASSERT_EQ(variable_uuids.at(i), constraint.variables().at(i));
+    }
+  }
+}
+
+TEST(Constraint, Type)
+{
+  fuse_core::UUID variable_uuid1 = fuse_core::uuid::generate();
+  TestConstraint constraint{variable_uuid1};
+  ASSERT_EQ("TestConstraint", constraint.type());
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ fuse_core/test/test_variable.cpp
@@ -0,0 +1,78 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <fuse_core/macros.h>
+#include <fuse_core/uuid.h>
+#include <fuse_core/variable.h>
+
+#include <gtest/gtest.h>
+
+
+/**
+ * @brief Dummy variable implementation for testing
+ */
+class TestVariable : public fuse_core::Variable
+{
+public:
+  SMART_PTR_DEFINITIONS(TestVariable);
+
+  TestVariable() :
+    data_(0.0),
+    uuid_(fuse_core::uuid::NIL)
+  {
+  }
+
+  fuse_core::UUID uuid() const override { return uuid_; }
+  size_t size() const override { return 1; }
+  const double* data() const override { return &data_; };
+  double* data() override { return &data_; };
+  void print(std::ostream& stream = std::cout) const override {}
+  fuse_core::Variable::UniquePtr clone() const override { return TestVariable::make_unique(*this); }
+
+private:
+  double data_;
+  fuse_core::UUID uuid_;
+};
+
+
+TEST(Variable, Type)
+{
+  TestVariable variable;
+  ASSERT_EQ("TestVariable", variable.type());
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
