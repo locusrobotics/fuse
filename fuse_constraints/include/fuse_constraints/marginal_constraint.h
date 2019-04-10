94fb8f493d36f326f4fbb4784de93cec925c353a
Stephen Williams <swilliams@locusrobotics.com>
2019-04-09 18:03:10 -0700

    Added a marginal constraint class

---
+++ fuse_constraints/include/fuse_constraints/marginal_constraint.h
@@ -0,0 +1,219 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef FUSE_CONSTRAINTS_MARGINAL_CONSTRAINT_H
+#define FUSE_CONSTRAINTS_MARGINAL_CONSTRAINT_H
+
+#include <fuse_core/constraint.h>
+#include <fuse_core/eigen.h>
+#include <fuse_core/local_parameterization.h>
+#include <fuse_core/macros.h>
+#include <fuse_core/variable.h>
+
+#include <boost/iterator/transform_iterator.hpp>
+#include <ceres/cost_function.h>
+
+#include <cassert>
+#include <ostream>
+#include <vector>
+
+
+namespace fuse_constraints
+{
+
+/**
+ * @brief A constraint that represents remaining marginal information on a set of variables
+ *
+ * The marginal constraint cost function is of the form:
+ *   cost = A1 * (x1 - x1_bar) + A2 * (x2 - x2_bar) + ... + b
+ * where x_bar is the linearization point of the variable taken from the variable value at the time of construction,
+ * and the minus operator in implemented in the variable's local parameterization.
+ */
+class MarginalConstraint : public fuse_core::Constraint
+{
+public:
+  SMART_PTR_DEFINITIONS(MarginalConstraint);
+
+  /**
+   * @brief Create a linear/marginal constraint
+   *
+   * The variable iterators and matrix iterators must be the same size. Further, all A matrices and the b vector must
+   * have the same number of rows, and the number of columns of each A matrix must match the \p localSize() of its
+   * associated variable.
+   *
+   * @param[in] first_variable Iterator pointing to the first involved variable for this constraint
+   * @param[in] last_variable  Iterator pointing to one passed the last involved variable for this constraint
+   * @param[in] first_A        Iterator pointing to the first A matrix, associated with the first variable
+   * @param[in] last_A         Iterator pointing to one passed the last A matrix
+   * @param[in] b              The b vector of the marginal cost (of the form A*(x - x_bar) + b)
+   */
+  template<typename VariableIterator, typename MatrixIterator>
+  MarginalConstraint(
+    VariableIterator first_variable,
+    VariableIterator last_variable,
+    MatrixIterator first_A,
+    MatrixIterator last_A,
+    const fuse_core::VectorXd& b);
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~MarginalConstraint();
+
+  /**
+   * @brief Read-only access to the A matrices of the marginal constraint
+   */
+  const std::vector<fuse_core::MatrixXd>& A() const { return A_; }
+
+  /**
+   * @brief Read-only access to the b vector of the marginal constraint
+   */
+  const fuse_core::VectorXd& b() const { return b_; }
+
+  /**
+   * @brief Read-only access to the variable linearization points, x_bar
+   */
+  const std::vector<fuse_core::VectorXd>& x_bar() const { return x_bar_; }
+
+  /**
+   * @brief Read-only access to the variable local parameterizations
+   */
+  const std::vector<fuse_core::LocalParameterization*>& localParameterizations() const { return local_; }
+
+  /**
+   * @brief Print a human-readable description of the constraint to the provided stream.
+   *
+   * @param[out] stream The stream to write to. Defaults to stdout.
+   */
+  void print(std::ostream& stream = std::cout) const override;
+
+  /**
+   * @brief Perform a deep copy of the constraint and return a unique pointer to the copy
+   *
+   * Unique pointers can be implicitly upgraded to shared pointers if needed.
+   *
+   * @return A unique pointer to a new instance of the most-derived constraint
+   */
+  fuse_core::Constraint::UniquePtr clone() const override;
+
+  /**
+   * @brief Construct an instance of this constraint's cost function
+   *
+   * The function caller will own the new cost function instance. It is the responsibility of the caller to delete
+   * the cost function object when it is no longer needed. If the pointer is provided to a Ceres::Problem object, the
+   * Ceres::Problem object will takes ownership of the pointer and delete it during destruction.
+   *
+   * @return A base pointer to an instance of a derived CostFunction.
+   */
+  ceres::CostFunction* costFunction() const override;
+
+protected:
+  std::vector<fuse_core::MatrixXd> A_;  //!< The A matrices of the marginal constraint
+  std::vector<fuse_core::VectorXd> x_bar_;  //!< The linearization point of each involved variable
+  std::vector<fuse_core::LocalParameterization*> local_;  //!< The local parameterization of each involved variable
+  fuse_core::VectorXd b_;  //!< The b vector of the marginal constraint
+};
+
+namespace detail
+{
+/**
+ * @brief Return the UUID of the provided variable
+ */
+const fuse_core::UUID getUuid(const fuse_core::Variable& variable)
+{
+  return variable.uuid();
+}
+
+/**
+ * @brief Return the current value of the provided variable
+ */
+const fuse_core::VectorXd getCurrentValue(const fuse_core::Variable& variable)
+{
+  return Eigen::Map<const fuse_core::VectorXd>(variable.data(), variable.size());
+}
+
+/**
+ * @brief Return the local parameterization of the provided variable
+ */
+fuse_core::LocalParameterization* const getLocalParameterization(const fuse_core::Variable& variable)
+{
+  return variable.localParameterization();
+}
+
+/**
+ * @brief Checks if the binary predicate returns true for all elements in the provided ranges
+ */
+template <class InputIt1, class InputIt2, class BinaryPredicate>
+constexpr bool all_of(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, BinaryPredicate pred)
+{
+  for (; first1 != last1; ++first1, first2)
+  {
+    if (!pred(*first1, *first2))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+}  // namespace detail
+
+template<typename VariableIterator, typename MatrixIterator>
+MarginalConstraint::MarginalConstraint(
+  VariableIterator first_variable,
+  VariableIterator last_variable,
+  MatrixIterator first_A,
+  MatrixIterator last_A,
+  const fuse_core::VectorXd& b) :
+    Constraint(boost::make_transform_iterator(first_variable, &detail::getUuid),
+               boost::make_transform_iterator(last_variable, &detail::getUuid)),
+    A_(first_A, last_A),
+    x_bar_(boost::make_transform_iterator(first_variable, &detail::getCurrentValue),
+           boost::make_transform_iterator(last_variable, &detail::getCurrentValue)),
+    local_(boost::make_transform_iterator(first_variable, &detail::getLocalParameterization),
+           boost::make_transform_iterator(last_variable, &detail::getLocalParameterization)),
+    b_(b)
+{
+  assert(A_.size() > 0);
+  assert(A_.size() == x_bar_.size());
+  assert(A_.size() == local_.size());
+  assert(b_.rows() > 0);
+  assert(std::all_of(A_.begin(), A_.end(), [&b_](const auto& A){ return A.rows() == b_.rows(); }));  // NOLINT
+  assert(detail::all_of(A_.begin(), A_.end(),
+                        first_variable, last_variable,
+                        [](const auto& A, const auto& x){ return A.cols() == x.localSize(); }));  // NOLINT
+}
+
+}  // namespace fuse_constraints
+
+#endif  // FUSE_CONSTRAINTS_MARGINAL_CONSTRAINT_H
---
+++ fuse_constraints/src/marginal_cost_function.cpp
@@ -0,0 +1,112 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <fuse_constraints/marginal_cost_function.h>
+
+#include <Eigen/Core>
+
+#include <vector>
+#include <iostream>
+
+
+namespace fuse_constraints
+{
+
+MarginalCostFunction::MarginalCostFunction(
+    const std::vector<fuse_core::MatrixXd>& A,
+    const std::vector<fuse_core::VectorXd>& x_bar,
+    const std::vector<fuse_core::LocalParameterization*>& local,
+    const fuse_core::VectorXd& b) :
+  A_(A),
+  x_bar_(x_bar),
+  local_(local),
+  b_(b)
+{
+  set_num_residuals(b_.rows());
+  for (const auto& x_bar : x_bar_)
+  {
+    mutable_parameter_block_sizes()->push_back(x_bar.size());
+  }
+}
+
+bool MarginalCostFunction::Evaluate(
+  double const* const* parameters,
+  double* residuals,
+  double** jacobians) const
+{
+  // Compute cost
+  Eigen::Map<fuse_core::VectorXd> residuals_map(residuals, num_residuals());
+  residuals_map = b_;
+  for (size_t i = 0; i < A_.size(); ++i)
+  {
+    fuse_core::VectorXd delta(A_[i].cols());
+    if (local_[i])
+    {
+      local_[i]->Minus(x_bar_[i].data(), parameters[i], delta.data());
+    }
+    else
+    {
+      for (int j = 0; j < x_bar_[i].rows(); ++j)
+      {
+        delta[j] = parameters[i][j] - x_bar_[i][j];
+      }
+    }
+    residuals_map += A_[i] * delta;
+  }
+
+  // Compute requested Jacobians
+  if (jacobians)
+  {
+    for (size_t i = 0; i < A_.size(); ++i)
+    {
+      if (jacobians[i])
+      {
+        if (local_[i])
+        {
+          const auto local = local_[i];
+          fuse_core::MatrixXd J_local(local->LocalSize(), local->GlobalSize());
+          local->ComputeMinusJacobian(parameters[i], J_local.data());
+          Eigen::Map<fuse_core::MatrixXd>(jacobians[i], num_residuals(), parameter_block_sizes()[i]) = A_[i] * J_local;
+        }
+        else
+        {
+          Eigen::Map<fuse_core::MatrixXd>(jacobians[i], num_residuals(), parameter_block_sizes()[i]) = A_[i];
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+}  // namespace fuse_constraints
