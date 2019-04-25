7e31194157551356e8519988a035d20df2307c41
Stephen Williams <swilliams@locusrobotics.com>
2019-04-25 16:20:52 -0700

    Added additional license disclaimers

--- fuse_constraints/src/marginalize_variables.cpp
+++ fuse_constraints/src/marginalize_variables.cpp
@@ -154,6 +154,45 @@ UuidOrdering computeEliminationOrder(
   return elimination_order;
 }
 
+/**
+ * In order for the linearize function to work correctly, it must perform the same operations as Google Ceres-Solver.
+ * Unfortunately those functions are not callable from the public API, so we must replicate them here. The following
+ * function is not identical to the Ceres-Solver code, but it was referenced heavily during the creation of this
+ * function. As such, acknowledging the original authors and license seems reasonable.
+ *
+ *  - https://github.com/ceres-solver/ceres-solver/blob/master/internal/ceres/residual_block.cc
+ *  - https://github.com/ceres-solver/ceres-solver/blob/master/internal/ceres/corrector.cc
+ */
+// Ceres Solver - A fast non-linear least squares minimizer
+// Copyright 2015 Google Inc. All rights reserved.
+// http://ceres-solver.org/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of Google Inc. nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: keir@google.com (Keir Mierle)
+//         sameeragarwal@google.com (Sameer Agarwal)
 LinearTerm linearize(
   const fuse_core::Constraint& constraint,
   const fuse_core::Graph& graph,
