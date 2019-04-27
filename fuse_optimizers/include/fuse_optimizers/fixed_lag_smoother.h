072a88ec82bb253ac6c5c2572b7d1b70da33ba01
Stephen Williams <swilliams@locusrobotics.com>
2019-04-26 20:49:04 -0700

    Copied the BatchOptimizer implementation to FixedLagSmoother as a starting point

---
+++ fuse_optimizers/include/fuse_optimizers/fixed_lag_smoother.h
@@ -0,0 +1,218 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef FUSE_OPTIMIZERS_FIXED_LAG_SMOOTHER_H
+#define FUSE_OPTIMIZERS_FIXED_LAG_SMOOTHER_H
+
+#include <fuse_core/graph.h>
+#include <fuse_core/macros.h>
+#include <fuse_core/transaction.h>
+#include <fuse_optimizers/optimizer.h>
+#include <ros/ros.h>
+
+#include <atomic>
+#include <condition_variable>
+#include <map>
+#include <mutex>
+#include <set>
+#include <shared_mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+
+namespace fuse_optimizers
+{
+
+/**
+ * @brief A simple optimizer implementation that uses batch optimization
+ *
+ * The batch optimization takes place in a separate thread. Received sensor transactions are queued while the
+ * optimization is processing, then applied to the graph at the start of the next optimization cycle. Optimization
+ * cycles are started at a fixed frequency. If the previous optimization is not yet complete when the optimization
+ * period elapses, then a new optimization will not be started. The previous optimization will run to completion, and
+ * the next optimization will not begin until the next scheduled optimization period. For batch optimization problems
+ * that continuously grow in size, this means that the optimization period is not overly important. The time spent
+ * waiting versus the time spent optimizing will approach zero as the problem size increases.
+ *
+ * Parameters:
+ *  - ignition_sensors (string list, default: "") The optimization will wait until a transaction is received from one
+ *                                                of these sensors. This is useful, for example, for providing an
+ *                                                initial guess of the robot's position and orientation. Any
+ *                                                transactions received before the ignition transaction will be deleted.
+ *                                                Leaving the \p ignition_sensors list empty will cause the optimization
+ *                                                to start immediately.
+ *  - motion_models (struct array) The set of motion model plugins to load
+ *    @code{.yaml}
+ *    - name: string  (A unique name for this motion model)
+ *      type: string  (The plugin loader class string for the desired motion model type)
+ *    - ...
+ *    @endcode
+ *  - optimization_period (float, default: 10.0) The minimum time delay, in seconds, between optimization cycles.
+ *  - publishers (struct array) The set of publisher plugins to load
+ *    @code{.yaml}
+ *    - name: string  (A unique name for this publisher)
+ *      type: string  (The plugin loader class string for the desired publisher type)
+ *    - ...
+ *    @endcode
+ *  - sensor_models (struct array) The set of sensor model plugins to load
+ *    @code{.yaml}
+ *    - name: string  (A unique name for this sensor model)
+ *      type: string  (The plugin loader class string for the desired sensor model type)
+ *      motion_models: [name1, name2, ...]  (An optional list of motion model names that should be applied)
+ *    - ...
+ *    @endcode
+ *  - transaction_timeout (float, default: 10.0) The maximum time to wait for motion models to be generated for a
+ *                                               received transactions. Transactions are processes sequentially, so
+ *                                               no new transactions will be added to the graph while waiting for
+ *                                               motion models to be generated. Once the timeout expires, that
+ *                                               transaction will be deleted from the queue.
+ */
+class FixedLagSmoother : public Optimizer
+{
+public:
+  SMART_PTR_DEFINITIONS(FixedLagSmoother);
+
+  /**
+   * @brief Constructor
+   *
+   * @param[in] graph               The derived graph object. This allows different graph implementations to be used
+   *                                with the same optimizer code.
+   * @param[in] node_handle         A node handle in the global namespace
+   * @param[in] private_node_handle A node handle in the node's private namespace
+   */
+  FixedLagSmoother(
+    fuse_core::Graph::UniquePtr graph,
+    const ros::NodeHandle& node_handle = ros::NodeHandle(),
+    const ros::NodeHandle& private_node_handle = ros::NodeHandle("~"));
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~FixedLagSmoother();
+
+protected:
+  /**
+   * Structure containing the information required to process a transaction after it was received.
+   */
+  struct TransactionQueueElement
+  {
+    std::string sensor_name;
+    fuse_core::Transaction::SharedPtr transaction;
+
+    TransactionQueueElement(
+      const std::string& sensor_name,
+      fuse_core::Transaction::SharedPtr transaction) :
+        sensor_name(sensor_name),
+        transaction(std::move(transaction)) {}
+  };
+
+  /**
+   * @brief Queue of Transaction objects, sorted by timestamp.
+   *
+   * Note: Because the queue is sorted on insert, the insert operation is O(logN). However, it is far more likely that
+   * the computer will run out of memory before the insertion time grows large enough to surpass the sensor update
+   * period.
+   */
+  using TransactionQueue = std::multimap<ros::Time, TransactionQueueElement>;
+
+  fuse_core::Transaction::SharedPtr combined_transaction_;  //!< Transaction used aggregate constraints and variables
+                                                            //!< from multiple sensors and motions models before being
+                                                            //!< applied to the graph.
+  std::mutex combined_transaction_mutex_;  //!< Synchronize access to the combined transaction across different threads
+  std::vector<std::string> ignition_sensors_;  //!< The set of sensors whose transactions will trigger the optimizer
+                                               //!< thread to start running. This is designed to keep the system idle
+                                               //!< until the origin constraint has been received.
+  std::atomic<bool> optimization_request_;  //!< Flag to trigger a new optimization
+  std::condition_variable optimization_requested_;  //!< Condition variable used by the optimization thread to wait
+                                                    //!< until a new optimization is requested by the main thread
+  std::mutex optimization_requested_mutex_;  //!< Required condition variable mutex
+  std::thread optimization_thread_;  //!< Thread used to run the optimizer as a background process
+  ros::Timer optimize_timer_;  //!< Trigger an optimization operation at a fixed frequency
+  TransactionQueue pending_transactions_;  //!< The set of received transactions that have not been added to the
+                                           //!< optimizer yet. Transactions are added by the main thread, and removed
+                                           //!< and processed by the optimization thread.
+  std::mutex pending_transactions_mutex_;  //!< Synchronize modification of the pending_transactions_ container
+  ros::Time start_time_;  //!< The timestamp of the first ignition sensor transaction
+  bool started_;  //!< Flag indicating the optimizer is ready/has received a transaction from an ignition sensor
+  ros::Duration transaction_timeout_;  //!< Parameter that controls how long to wait for a transaction to be processed
+                                       //!< successfully before kicking it out of the queue.
+
+  /**
+   * @brief Generate motion model constraints for pending transactions
+   *
+   * Transactions are processed sequentially based on timestamp. If motion models are successfully generated for a
+   * pending transactions, that transaction is merged into the combined_transaction_ variable and removed from the
+   * pending queue. If motion models fail to generate after the configured transaction_timeout_, the transaction
+   * will be deleted from the pending queue and a warning will be displayed.
+   */
+  void applyMotionModelsToQueue();
+
+  /**
+   * @brief Function that optimizes all constraints, designed to be run in a separate thread.
+   *
+   * This function waits for an optimization or shutdown signal, then either calls optimize() or exits appropriately.
+   */
+  void optimizationLoop();
+
+  /**
+   * @brief Callback fired at a fixed frequency to trigger a new optimization cycle.
+   *
+   * This callback checks if a current optimization cycle is still running. If not, a new optimization cycle is started.
+   * If so, we simply wait for the next timer event to start another optimization cycle.
+   *
+   * @param event  The ROS timer event metadata
+   */
+  void optimizerTimerCallback(const ros::TimerEvent& event);
+
+  /**
+   * @brief Callback fired every time the SensorModel plugin creates a new transaction
+   *
+   * This callback is responsible for ensuring all associated motion models are applied before any other processing
+   * takes place. See Optimizer::applyMotionModels() for a helper function that does just that.
+   *
+   * This implementation shares ownership of the transaction object.
+   *
+   * @param[in] name        The name of the sensor that produced the Transaction
+   * @param[in] stamps      Any timestamps associated with the added variables. These are sent to the motion models
+   *                        to generate connected constraints.
+   * @param[in] transaction The populated Transaction object created by the loaded SensorModel plugin
+   */
+  void transactionCallback(
+    const std::string& sensor_name,
+    fuse_core::Transaction::SharedPtr transaction) override;
+};
+
+}  // namespace fuse_optimizers
+
+#endif  // FUSE_OPTIMIZERS_FIXED_LAG_SMOOTHER_H
---
+++ fuse_optimizers/src/fixed_lag_smoother.cpp
@@ -0,0 +1,272 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <fuse_optimizers/fixed_lag_smoother.h>
+
+#include <fuse_core/transaction.h>
+#include <fuse_optimizers/optimizer.h>
+#include <ros/ros.h>
+
+#include <algorithm>
+#include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <thread>
+
+
+namespace fuse_optimizers
+{
+
+FixedLagSmoother::FixedLagSmoother(
+  fuse_core::Graph::UniquePtr graph,
+  const ros::NodeHandle& node_handle,
+  const ros::NodeHandle& private_node_handle) :
+    fuse_optimizers::Optimizer(std::move(graph), node_handle, private_node_handle),
+    combined_transaction_(fuse_core::Transaction::make_shared()),
+    optimization_request_(false),
+    start_time_(ros::TIME_MAX),
+    started_(false)
+{
+  double optimization_period;
+  double default_optimization_period = 10.0;
+  private_node_handle_.param("optimization_period", optimization_period, default_optimization_period);
+  if (optimization_period <= 0)
+  {
+    ROS_WARN_STREAM("The requested optimization_period is <= 0. Using the default value (" <<
+                    default_optimization_period << "s) instead.");
+    optimization_period = default_optimization_period;
+  }
+
+  double transaction_timeout;
+  double default_transaction_timeout = 10.0;
+  private_node_handle_.param("transaction_timeout", transaction_timeout, default_transaction_timeout);
+  if (transaction_timeout <= 0)
+  {
+    ROS_WARN_STREAM("The requested transaction_timeout is <= 0. Using the default value (" <<
+                    default_transaction_timeout << "s) instead.");
+    transaction_timeout = default_transaction_timeout;
+  }
+
+  private_node_handle_.getParam("ignition_sensors", ignition_sensors_);
+  if (ignition_sensors_.empty())
+  {
+    // No ignition sensors were provided. Auto-start.
+    started_ = true;
+    start_time_ = ros::Time(0, 0);
+    ROS_INFO_STREAM("No ignition sensors were specified. Optimization will begin immediately.");
+  }
+  else
+  {
+    for (const auto& sensor_model_name : ignition_sensors_)
+    {
+      if (sensor_models_.find(sensor_model_name) == sensor_models_.end())
+      {
+        ROS_WARN_STREAM("Sensor '" << sensor_model_name << "' is configured as an ignition sensor, but no sensor "
+                        "model with that name currently exists. This is likely a configuration error.");
+      }
+    }
+    // Sort the sensors for efficient lookups
+    std::sort(ignition_sensors_.begin(), ignition_sensors_.end());
+  }
+
+  // Configure a timer to trigger optimizations
+  optimize_timer_ = node_handle_.createTimer(
+    ros::Duration(optimization_period),
+    &FixedLagSmoother::optimizerTimerCallback,
+    this);
+
+  // Start the optimization thread
+  optimization_thread_ = std::thread(&FixedLagSmoother::optimizationLoop, this);
+}
+
+FixedLagSmoother::~FixedLagSmoother()
+{
+  // Wake up any sleeping threads
+  optimization_requested_.notify_all();
+  // Wait for the threads to shutdown
+  if (optimization_thread_.joinable())
+  {
+    optimization_thread_.join();
+  }
+}
+
+void FixedLagSmoother::applyMotionModelsToQueue()
+{
+  // We need get the pending transactions from the queue
+  std::lock_guard<std::mutex> pending_transactions_lock(pending_transactions_mutex_);
+  // Use the most recent transaction time as the current time
+  ros::Time current_time(0, 0);
+  if (!pending_transactions_.empty())
+  {
+    current_time = pending_transactions_.rbegin()->first;
+  }
+  // Attempt to process each pending transaction
+  while (!pending_transactions_.empty())
+  {
+    auto& element = pending_transactions_.begin()->second;
+    // Apply the motion models to the transaction
+    if (!applyMotionModels(element.sensor_name, *element.transaction))
+    {
+      if (element.transaction->stamp() + transaction_timeout_ < current_time)
+      {
+        // Warn that this transaction has expired, then skip it.
+        ROS_ERROR_STREAM("The queued transaction with timestamp " << element.transaction->stamp()
+                          << " could not be processed after " << (current_time - element.transaction->stamp())
+                          << " seconds, which is greater than the 'transaction_timeout' value of "
+                          << transaction_timeout_ << ". Ignoring this transaction.");
+        pending_transactions_.erase(pending_transactions_.begin());
+        continue;
+      }
+      else
+      {
+        // Stop processing future transactions. Try again next time.
+        break;
+      }
+    }
+    // Merge the sensor+motion model transactions into a combined transaction that will be applied directly to the graph
+    {
+      std::lock_guard<std::mutex> combined_transaction_lock(combined_transaction_mutex_);
+      combined_transaction_->merge(*element.transaction, true);
+    }
+    // We are done with this transaction. Delete it from the queue.
+    pending_transactions_.erase(pending_transactions_.begin());
+  }
+}
+
+void FixedLagSmoother::optimizationLoop()
+{
+  // Optimize constraints until told to exit
+  while (ros::ok())
+  {
+    // Wait for the next signal to start the next optimization cycle
+    {
+      std::unique_lock<std::mutex> lock(optimization_requested_mutex_);
+      optimization_requested_.wait(lock, [this]{ return optimization_request_ || !ros::ok(); });  // NOLINT
+    }
+    // If a shutdown is requested, exit now.
+    if (!ros::ok())
+    {
+      break;
+    }
+    // Copy the combined transaction so it can be shared with all the plugins
+    fuse_core::Transaction::ConstSharedPtr const_transaction;
+    {
+      std::lock_guard<std::mutex> lock(combined_transaction_mutex_);
+      const_transaction = std::move(combined_transaction_);
+      combined_transaction_ = fuse_core::Transaction::make_shared();
+    }
+    // Update the graph
+    graph_->update(*const_transaction);
+    // Optimize the entire graph
+    graph_->optimize();
+    // Make a copy of the graph to share
+    fuse_core::Graph::ConstSharedPtr const_graph = graph_->clone();
+    // Optimization is complete. Notify all the things about the graph changes.
+    notify(const_transaction, const_graph);
+    // Clear the request flag now that this optimization cycle is complete
+    optimization_request_ = false;
+  }
+}
+
+void FixedLagSmoother::optimizerTimerCallback(const ros::TimerEvent& event)
+{
+  // If an "ignition" transaction hasn't been received, then we can't do anything yet.
+  if (!started_)
+  {
+    return;
+  }
+  // Attempt to generate motion models for any queued transactions
+  applyMotionModelsToQueue();
+  // Check if there is any pending information to be applied to the graph.
+  {
+    std::lock_guard<std::mutex> lock(combined_transaction_mutex_);
+    optimization_request_ = !combined_transaction_->addedConstraints().empty() ||
+                            !combined_transaction_->removedConstraints().empty() ||
+                            !combined_transaction_->addedVariables().empty() ||
+                            !combined_transaction_->removedVariables().empty();
+  }
+  // If there is some pending work, trigger the next optimization cycle.
+  // If the optimizer has not completed the previous optimization cycle, then it
+  // will not be waiting on the condition variable signal, so nothing will happen.
+  if (optimization_request_)
+  {
+    optimization_requested_.notify_one();
+  }
+}
+
+void FixedLagSmoother::transactionCallback(
+  const std::string& sensor_name,
+  fuse_core::Transaction::SharedPtr transaction)
+{
+  // Add the new transaction to the pending set
+  // Either we haven't "started" yet and we want to keep a short history of transactions around
+  // Or we have "started" already, and the new transaction is after the starting time.
+  ros::Time transaction_time = transaction->stamp();
+  ros::Time last_pending_time(0, 0);
+  if (!started_ || transaction_time >= start_time_)
+  {
+    std::lock_guard<std::mutex> lock(pending_transactions_mutex_);
+    pending_transactions_.emplace(transaction_time, TransactionQueueElement(sensor_name, std::move(transaction)));
+    last_pending_time = pending_transactions_.rbegin()->first;
+  }
+  // If we haven't "started" yet...
+  if (!started_)
+  {
+    // Check if this transaction "starts" the system
+    if (std::binary_search(ignition_sensors_.begin(), ignition_sensors_.end(), sensor_name))
+    {
+      started_ = true;
+      start_time_ = transaction_time;
+    }
+    // Purge old transactions from the pending queue
+    ros::Time purge_time(0, 0);
+    if (started_)
+    {
+      purge_time = start_time_;
+    }
+    else if (ros::Time(0, 0) + transaction_timeout_ < last_pending_time)  // taking care to prevent a bad subtraction
+    {
+      purge_time = last_pending_time - transaction_timeout_;
+    }
+    std::lock_guard<std::mutex> lock(pending_transactions_mutex_);
+    auto purge_iter = pending_transactions_.lower_bound(purge_time);
+    pending_transactions_.erase(pending_transactions_.begin(), purge_iter);
+  }
+  // If we have "started", attempt to process any pending transactions
+  if (started_)
+  {
+    applyMotionModelsToQueue();
+  }
+}
+
+}  // namespace fuse_optimizers
