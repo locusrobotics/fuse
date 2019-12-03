4ff419d9021ef1495a5f9ad9cfce43fadd8547cd
Enrique Fernandez <efernandez@clearpath.ai>
2019-12-03 16:01:18 +0100

    Update copyright

--- fuse_core/src/loss.cpp
+++ fuse_core/src/loss.cpp
@@ -1,7 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2018, Locus Robotics
+ *  Copyright (c) 2019, Clearpath Robotics
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
