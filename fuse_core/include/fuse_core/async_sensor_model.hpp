c0bb3f71d20bd46210869bed49b2ec46f87bf1da
methylDragon <methylDragon@gmail.com>
2022-12-06 12:59:14 -0800

    Update docstrings for async classes
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- fuse_core/include/fuse_core/async_sensor_model.hpp
+++ fuse_core/include/fuse_core/async_sensor_model.hpp
@@ -47,22 +47,22 @@ namespace fuse_core
 {
 
 /**
- * @brief A sensor model base class that provides node handles and a private callback queue.
+ * @brief A sensor model base class that provides an internal node and an internal callback queue.
  *
  * A sensor model plugin is responsible for generating new constraints, packaging them in a
  * fuse_core::Transaction object, and passing them along to the optimizer. The asynchronous
- * sensor model plugin is designed similar to the nodelet plugin, attempting to be as generic and
+ * sensor model plugin is designed similar to a nodelet plugin, attempting to be as generic and
  * easy to use as a standard ROS node.
  *
  * There are a few notable differences between the asynchronous sensor model plugin and a
  * standard ROS node. First and most obvious, the sensor model is designed as a plugin, with all
  * of the stipulations and requirements that come with all ROS plugins (must be derived from a
  * known base class, will be default constructed). Second, the base AsyncSensorModel class
- * provides a global and private node handle, both hooked to a local callback queue and local
- * spinner. This makes it act like a full ROS node -- subscriptions trigger message callbacks,
+ * provides an internal node that is hooked to a local callback queue and local executor on
+ * init. This makes it act like a full ROS node -- subscriptions trigger message callbacks,
  * callbacks will fire sequentially, etc. However, authors of derived sensor models should be
- * aware of this fact and avoid creating additional node handles, or at least take care when
- * creating new node handles and additional callback queues. Finally, instead of publishing the
+ * aware of this fact and avoid creating additional sub-nodes, or at least take care when
+ * creating new sub-nodes and additional callback queues. Finally, instead of publishing the
  * generated constraints using a ROS publisher, the asynchronous sensor model provides a
  * "sendTransaction()" method that should be called whenever the sensor is ready to send a
  * fuse_core::Transaction object to the Optimizer. Under the hood, this executes the
@@ -103,9 +103,9 @@ public:
    * This method will be called by the optimizer, in the optimizer's thread, after each Graph
    * update is complete. This implementation repackages the provided \p graph, and inserts a
    * call to onGraphUpdate() into this sensor's callback queue. This is meant to simplify
-   * thread synchronization. If this sensor uses a single-threaded spinner, then all callbacks
+   * thread synchronization. If this sensor uses a single-threaded executor, then all callbacks
    * will fire sequentially and no semaphores are needed. If this sensor uses a multi-threaded
-   * spinner, then normal multithreading rules apply and data accessed in more than one place
+   * executor, then normal multithreading rules apply and data accessed in more than one place
    * should be guarded.
    *
    * @param[in] graph A read-only pointer to the graph object, allowing queries to be performed
@@ -118,12 +118,13 @@ public:
    *        reading from the parameter server.
    *
    * This will be called for each plugin after construction and after the ROS node has been
-   * initialized. The provided private node handle will be in a namespace based on the plugin's
-   * name. This should prevent conflicts and allow the same plugin to be used multiple times
-   * with different settings and topics.
+   * initialized. The provided node will be in a namespace based on the plugin's name. This should
+   * prevent conflicts and allow the same plugin to be used multiple times with different settings
+   * and topics.
    *
    * @param[in] name                 A unique name to give this plugin instance
    * @param[in] transaction_callback The function to call every time a transaction is published
+   * @throws runtime_error if already initialized
    */
   void initialize(
     const std::string & name,
@@ -162,9 +163,9 @@ public:
    *
    * This implementation inserts a call to onStart() into this sensor model's callback queue.
    * This method then blocks until the call to onStart() has completed. This is meant to
-   * simplify thread synchronization. If this sensor model uses a single-threaded spinner, then
+   * simplify thread synchronization. If this sensor model uses a single-threaded executor, then
    * all callbacks will fire sequentially and no semaphores are needed. If this sensor model
-   * uses a multithreaded spinner, then normal multithreading rules apply and data accessed in
+   * uses a multithreaded executor, then normal multithreading rules apply and data accessed in
    * more than one place should be guarded.
    */
   void start() override;
@@ -180,9 +181,9 @@ public:
    *
    * This implementation inserts a call to onStop() into this sensor model's callback queue.
    * This method then blocks until the call to onStop() has completed. This is meant to
-   * simplify thread synchronization. If this sensor model uses a single-threaded spinner, then
+   * simplify thread synchronization. If this sensor model uses a single-threaded executor, then
    * all callbacks will fire sequentially and no semaphores are needed. If this sensor model
-   * uses a multithreaded spinner, then normal multithreading rules apply and data accessed in
+   * uses a multithreaded executor, then normal multithreading rules apply and data accessed in
    * more than one place should be guarded.
    */
   void stop() override;
@@ -195,7 +196,7 @@ protected:
   rclcpp::Node::SharedPtr node_;  //!< The node for this sensor model
   rclcpp::CallbackGroup::SharedPtr cb_group_;  //!< Internal re-entrant callback group
 
-  //! A single/multi-threaded spinner assigned to the local callback queue
+  //! A single/multi-threaded executor assigned to the local callback queue
   rclcpp::executors::MultiThreadedExecutor::SharedPtr executor_;
 
   //! The function to be executed every time a Transaction is "published"
@@ -207,7 +208,7 @@ protected:
   /**
    * @brief Constructor
    *
-   * Construct a new sensor model and create a local callback queue and thread spinner.
+   * Construct a new sensor model and create a local callback queue and internal executor.
    *
    * @param[in] thread_count The number of threads used to service the local callback queue
    */
@@ -236,7 +237,7 @@ protected:
    * @brief Perform any required initialization for the sensor model
    *
    * This could include things like reading from the parameter server or subscribing to topics.
-   * The class's node handles will be properly initialized before onInit() is called. Spinning
+   * The class's node will be properly initialized before onInit() is called. Spinning
    * of the callback queue will not begin until after the call to onInit() completes.
    *
    * Derived sensor models classes must implement this function, because otherwise I'm not sure
@@ -266,7 +267,7 @@ protected:
   virtual void onStop() {}
 
 private:
-  //! Stop the internal executor thread (in order to use this again it must be re-initialized)
+  //! Stop the internal executor thread (in order to use this class again it must be re-initialized)
   void internal_stop();
 };
 
