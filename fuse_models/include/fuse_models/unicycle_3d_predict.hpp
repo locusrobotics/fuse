55280dc4ca07141376d24c45f5963f8452281aaf
giacomo franchini <giacomo.franchini@polito.it>
2023-11-28 14:02:30 +0000

    unicycle 3d predict refactor + test

--- fuse_models/include/fuse_models/unicycle_3d_predict.hpp
+++ fuse_models/include/fuse_models/unicycle_3d_predict.hpp
@@ -34,199 +34,135 @@
 #ifndef FUSE_MODELS__UNICYCLE_3D_PREDICT_HPP_
 #define FUSE_MODELS__UNICYCLE_3D_PREDICT_HPP_
 
-#include <ceres/jet.h>
 #include <Eigen/Core>
-#include <array>
 
 #include <fuse_core/util.hpp>
 #include <fuse_core/eigen.hpp>
 
 namespace fuse_models
 {
-using Jet = ceres::Jet<double, 32>;
 /**
- * @brief Given a state and time delta, predicts a new state - 
- *        inner templated version for ceres autodiff
- * @param[in] position1      - First position
- * @param[in] orientation1   - First orientation
- * @param[in] vel_linear1    - First linear velocity
- * @param[in] vel_angular1   - First angular velocity
- * @param[in] acc_linear1    - First linear acceleration
+ * @brief Given a state and time delta, predicts a new state
+ * @param[in] position1_x - First X position
+ * @param[in] position1_y - First Y position
+ * @param[in] position1_z - First Z position
+ * @param[in] orientation1_r - First roll orientation
+ * @param[in] orientation1_p - First pitch orientation
+ * @param[in] orientation1_y - First yaw orientation
+ * @param[in] vel_linear1_x - First X velocity
+ * @param[in] vel_linear1_y - First Y velocity
+ * @param[in] vel_linear1_z - First Z velocity
+ * @param[in] vel_angular1_r - First roll velocity
+ * @param[in] vel_angular1_p - First pitch velocity
+ * @param[in] vel_angular1_y - First yaw velocity
+ * @param[in] acc_linear1_x - First X acceleration
+ * @param[in] acc_linear1_y - First Y acceleration
+ * @param[in] acc_linear1_z - First Z acceleration
  * @param[in] dt - The time delta across which to predict the state
- * @param[out] position2     - Second position
- * @param[out] orientation2  - Second orientation
- * @param[out] vel_linear2   - Second linear velocity
- * @param[out] vel_angular2  - Second angular velocity
- * @param[out] acc_linear2   - Second linear acceleration
+ * @param[out] position2_x - Second X position
+ * @param[out] position2_y - Second Y position
+ * @param[out] position2_z - Second Z position
+ * @param[out] orientation2_r - Second roll orientation
+ * @param[out] orientation2_p - Second pitch orientation
+ * @param[out] orientation2_y - Second yaw orientation
+ * @param[out] vel_linear2_x - Second X velocity
+ * @param[out] vel_linear2_y - Second Y velocity
+ * @param[out] vel_linear2_z - Second Z velocity
+ * @param[out] vel_angular_r - Second roll velocity
+ * @param[out] vel_angular_p - Second pitch velocity
+ * @param[out] vel_angular_y - Second yaw velocity
+ * @param[out] acc_linear2_x - Second X acceleration
+ * @param[out] acc_linear2_y - Second Y acceleration
+ * @param[out] acc_linear2_z - Second Z acceleration
  */
-inline void predict_eigen(
-  const Eigen::Matrix<double, 3, 1>& position1,
-  const Eigen::Quaternion<double>& orientation1,
-  const Eigen::Matrix<double, 3, 1>& vel_linear1,
-  const Eigen::Matrix<double, 3, 1>& vel_angular1,
-  const Eigen::Matrix<double, 3, 1>& acc_linear1,
-  const double dt,
-  Eigen::Matrix<double, 3, 1>& position2,
-  Eigen::Quaternion<double>& orientation2,
-  Eigen::Matrix<double, 3, 1>& vel_linear2,
-  Eigen::Matrix<double, 3, 1>& vel_angular2,
-  Eigen::Matrix<double, 3, 1>& acc_linear2)
+template<typename T>
+inline void predict(
+  const T position1_x,
+  const T position1_y,
+  const T position1_z,
+  const T orientation1_r,
+  const T orientation1_p,
+  const T orientation1_y,
+  const T vel_linear1_x,
+  const T vel_linear1_y,
+  const T vel_linear1_z,
+  const T vel_angular1_r,
+  const T vel_angular1_p,
+  const T vel_angular1_y,
+  const T acc_linear1_x,
+  const T acc_linear1_y,
+  const T acc_linear1_z,
+  const T dt,
+  T & position2_x,  
+  T & position2_y,
+  T & position2_z,
+  T & orientation2_r,
+  T & orientation2_p,
+  T & orientation2_y,
+  T & vel_linear2_x,
+  T & vel_linear2_y,
+  T & vel_linear2_z,
+  T & vel_angular2_r,
+  T & vel_angular2_p,
+  T & vel_angular2_y,
+  T & acc_linear2_x,
+  T & acc_linear2_y,
+  T & acc_linear2_z)
 {
-  // Convert quaternion to eigen
-  Eigen::Vector<double, 3> rpy(
-    fuse_core::getRoll(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z()),
-    fuse_core::getPitch(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z()),
-    fuse_core::getYaw(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z())
-  );
-  fuse_core::wrapAngle2D(rpy.x());
-  fuse_core::wrapAngle2D(rpy.y());
-  fuse_core::wrapAngle2D(rpy.z());
   // 3D material point projection model which matches the one used by r_l.
-  double sr = ceres::sin(rpy.x());
-  double cr = ceres::cos(rpy.x());
-  double sp = ceres::sin(rpy.y());
-  double cp = ceres::cos(rpy.y());
-  double sy = ceres::sin(rpy.z());  // Should probably be sin((yaw1 + yaw2) / 2), but r_l uses this model
-  double cy = ceres::cos(rpy.z());
-  double cpi = 1.0 / cp;
-
-  Eigen::Matrix<double, 3, 3> tf_pos, tf_rot, tf_vel, tf_acc;
-  Eigen::Matrix<double, 15, 15> state_tf;
-
-  tf_pos(0, 0) = (cy * cp); // vx1 to x2
-  tf_pos(0, 1) = (cy * sp * sr - sy * cr); // vy1 to x2
-  tf_pos(0, 2) = (cy * sp * cr + sy * sr); // vz1 to x2
-  tf_pos(1, 0) = (sy * cp); // vx1 to y2
-  tf_pos(1, 1) = (sy * sp * sr + cy * cr); // vy1 to y2
-  tf_pos(1, 2) = (sy * sp * cr - cy * sr); // vz1 to y2
-  tf_pos(2, 0) = (-sp); // vx1 to z2
-  tf_pos(2, 1) = (cp * sr); // vy1 to z2
-  tf_pos(2, 2) = (cp * cr); // vz1 to z2
-
-  tf_rot(0, 0) = 1;
-  tf_rot(0, 1) = sr * sp * cpi;
-  tf_rot(0, 2) = cr * sp * cpi;
-  tf_rot(1, 0) = double(0);
-  tf_rot(1, 1) = cr;
-  tf_rot(1, 2) = -sr;
-  tf_rot(2, 0) = double(0);
-  tf_rot(2, 1) = sr * cpi;
-  tf_rot(2, 2) = cr * cpi;
+  T sr = ceres::sin(orientation1_r);
+  T cr = ceres::cos(orientation1_r);
+  T sp = ceres::sin(orientation1_p);
+  T cp = ceres::cos(orientation1_p);
+  T sy = ceres::sin(orientation1_y);
+  T cy = ceres::cos(orientation1_y);
+  T cpi = T(1.0) / cp;
 
   // Project the state
-  position2 = position1 + tf_pos * vel_linear1 * dt + 0.5 * tf_pos * acc_linear1 * dt * dt;
-  rpy = rpy + tf_rot * vel_angular1 * dt;
-  vel_linear2 = vel_linear1 + acc_linear1 * dt;
-  vel_angular2 = vel_angular1;
-  acc_linear2 = acc_linear1;
+  position2_x = position1_x + 
+    ((cy * cp) * vel_linear1_x + (cy * sp * sr - sy * cr) * vel_linear1_y + (cy * sp * cr + sy * sr) * vel_linear1_z) * dt +
+    ((cy * cp) * acc_linear1_x + (cy * sp * sr - sy * cr) * acc_linear1_y + (cy * sp * cr + sy * sr) * acc_linear1_z) * T(0.5) * dt * dt;
+  position2_y = position1_y +
+    ((sy * cp) * vel_linear1_x + (sy * sp * sr + cy * cr) * vel_linear1_y + (sy * sp * cr - cy * sr) * vel_linear1_z) * dt +
+    ((sy * cp) * acc_linear1_x + (sy * sp * sr + cy * cr) * acc_linear1_y + (sy * sp * cr - cy * sr) * acc_linear1_z) * T(0.5) * dt * dt;
+  position2_z = position1_z +
+    ((-sp) * vel_linear1_x + (cp * sr) + vel_linear1_y + (cp * cr) * vel_linear1_z) * dt +
+    ((-sp) * acc_linear1_x + (cp * sr) + acc_linear1_y + (cp * cr) * acc_linear1_z) * T(0.5) * dt * dt;
+  
+  orientation2_r = orientation1_r + 
+    (vel_angular1_r + sr * sp * cpi * vel_angular1_p + cr * sp * cpi * vel_angular1_y) * dt;
+  orientation2_p = orientation1_p + 
+    (cr * vel_angular1_p - sr * vel_angular1_y) * dt;
+  orientation2_y = orientation1_y + 
+    (sr * cpi * vel_angular1_p + cr * cpi * vel_angular1_y) * dt;
+  
+  vel_linear2_x = vel_linear1_x + acc_linear1_x * dt;
+  vel_linear2_y = vel_linear1_y + acc_linear1_y * dt;
+  vel_linear2_z = vel_linear1_z + acc_linear1_z * dt;
 
-  fuse_core::wrapAngle2D(rpy.x());
-  fuse_core::wrapAngle2D(rpy.y());
-  fuse_core::wrapAngle2D(rpy.z());
+  vel_angular2_r = vel_angular1_r;
+  vel_angular2_p = vel_angular1_p;
+  vel_angular2_y = vel_angular1_y;
 
-  // Convert back to quaternion
-  orientation2 = Eigen::AngleAxis<double>(rpy.z(), Eigen::Matrix<double, 3, 1>::UnitZ()) *
-                 Eigen::AngleAxis<double>(rpy.y(), Eigen::Matrix<double, 3, 1>::UnitY()) *
-                 Eigen::AngleAxis<double>(rpy.x(), Eigen::Matrix<double, 3, 1>::UnitX());
-}
+  acc_linear2_x = acc_linear1_x;
+  acc_linear2_y = acc_linear1_y;
+  acc_linear2_z = acc_linear1_z;
 
-/**
- * @brief Given a state and time delta, predicts a new state - 
- *        inner templated version for ceres autodiff
- * @param[in] position1      - First position
- * @param[in] orientation1   - First orientation
- * @param[in] vel_linear1    - First linear velocity
- * @param[in] vel_angular1   - First angular velocity
- * @param[in] acc_linear1    - First linear acceleration
- * @param[in] dt - The time delta across which to predict the state
- * @param[out] position2     - Second position
- * @param[out] orientation2  - Second orientation
- * @param[out] vel_linear2   - Second linear velocity
- * @param[out] vel_angular2  - Second angular velocity
- * @param[out] acc_linear2   - Second linear acceleration
- */
-inline void predict_eigen(
-  const Eigen::Matrix<Jet, 3, 1>& position1,
-  const Eigen::Quaternion<Jet>& orientation1,
-  const Eigen::Matrix<Jet, 3, 1>& vel_linear1,
-  const Eigen::Matrix<Jet, 3, 1>& vel_angular1,
-  const Eigen::Matrix<Jet, 3, 1>& acc_linear1,
-  const Jet dt,
-  Eigen::Matrix<Jet, 3, 1>& position2,
-  Eigen::Quaternion<Jet>& orientation2,
-  Eigen::Matrix<Jet, 3, 1>& vel_linear2,
-  Eigen::Matrix<Jet, 3, 1>& vel_angular2,
-  Eigen::Matrix<Jet, 3, 1>& acc_linear2)
-{
-  // Convert quaternion to eigen
-  Eigen::Vector<Jet, 3> rpy(
-    fuse_core::getRoll(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z()),
-    fuse_core::getPitch(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z()),
-    fuse_core::getYaw(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z())
-  );
-  fuse_core::wrapAngle2D(rpy.x());
-  fuse_core::wrapAngle2D(rpy.y());
-  fuse_core::wrapAngle2D(rpy.z());
-  // 3D material point projection model which matches the one used by r_l.
-  Jet sr = ceres::sin(rpy.x());
-  Jet cr = ceres::cos(rpy.x());
-  Jet sp = ceres::sin(rpy.y());
-  Jet cp = ceres::cos(rpy.y());
-  Jet sy = ceres::sin(rpy.z());  // Should probably be sin((yaw1 + yaw2) / 2), but r_l uses this model
-  Jet cy = ceres::cos(rpy.z());
-  Jet cpi = 1.0 / cp;
-
-  Eigen::Matrix<Jet, 3, 3> tf_pos, tf_rot, tf_vel, tf_acc;
-  Eigen::Matrix<Jet, 15, 15> state_tf;
-
-  tf_pos(0, 0) = (cy * cp);
-  tf_pos(0, 1) = (cy * sp * sr - sy * cr);
-  tf_pos(0, 2) = (cy * sp * cr + sy * sr);
-  tf_pos(1, 0) = (sy * cp);
-  tf_pos(1, 1) = (sy * sp * sr + cy * cr);
-  tf_pos(1, 2) = (sy * sp * cr - cy * sr);
-  tf_pos(2, 0) = (-sp);
-  tf_pos(2, 1) = (cp * sr);
-  tf_pos(2, 2) = (cp * cr);
-
-  tf_rot(0, 0) = Jet(1);
-  tf_rot(0, 1) = sr * sp * cpi;
-  tf_rot(0, 2) = cr * sp * cpi;
-  tf_rot(1, 0) = Jet(0);
-  tf_rot(1, 1) = cr;
-  tf_rot(1, 2) = -sr;
-  tf_rot(2, 0) = Jet(0);
-  tf_rot(2, 1) = sr * cpi;
-  tf_rot(2, 2) = cr * cpi;
-
-  // Project the state
-  position2 = position1 + tf_pos * vel_linear1 * dt + 0.5 * tf_pos * acc_linear1 * dt * dt;
-  rpy = rpy + tf_rot * vel_angular1 * dt;
-  vel_linear2 = vel_linear1 + acc_linear1 * dt;
-  vel_angular2 = vel_angular1;
-  acc_linear2 = acc_linear1;
-
-  fuse_core::wrapAngle2D(rpy.x());
-  fuse_core::wrapAngle2D(rpy.y());
-  fuse_core::wrapAngle2D(rpy.z());
-
-  // Convert back to quaternion
-  orientation2 = Eigen::AngleAxis<Jet>(rpy.z(), Eigen::Matrix<Jet, 3, 1>::UnitZ()) *
-                 Eigen::AngleAxis<Jet>(rpy.y(), Eigen::Matrix<Jet, 3, 1>::UnitY()) *
-                 Eigen::AngleAxis<Jet>(rpy.x(), Eigen::Matrix<Jet, 3, 1>::UnitX());
-}
+  fuse_core::wrapAngle2D(orientation2_r);
+  fuse_core::wrapAngle2D(orientation2_p);
+  fuse_core::wrapAngle2D(orientation2_y);
+  }
 
 /**
  * @brief Given a state and time delta, predicts a new state -
- *        Templated version to be called from ceres autodiff costfunction
  * @param[in] position1    - First position (array with x at index 0, y at index 1, z at index 2)
- * @param[in] orientation1 - First orientation (array with w at index 0, x at index 1, y at index 2, z at index 3) check this order
+ * @param[in] orientation1 - First orientation (array with roll at index 0, pitch at index 1, yaw at index 2)
  * @param[in] vel_linear1  - First linear velocity (array with x at index 0, y at index 1, z at index 2)
  * @param[in] vel_angular1 - First angular velocity (array with vroll at index 0, vpitch at index 1, vyaw at index 2)
  * @param[in] acc_linear1  - First linear acceleration (array with x at index 0, y at index 1, z at index 2)
  * @param[in] dt - The time delta across which to predict the state
- * @param[out] position2 - Second position (array with x at index 0, y at index 1, z at index 2)
- * @param[out] orientation2 - Second orientation (array with w at index 0, x at index 1, y at index 2, z at index 3) check this order
+ * @param[out] position2    - Second position (array with x at index 0, y at index 1, z at index 2)
+ * @param[out] orientation2 - Second orientation (array with roll at index 0, pitch at index 1, yaw at index 2)
  * @param[out] vel_linear2  - Second velocity (array with x at index 0, y at index 1, z at index 2)
  * @param[out] vel_angular2 - Second yaw velocity (array with vroll at index 0, vpitch at index 1, vyaw at index 2)
  * @param[out] acc_linear2  - Second linear acceleration (array with x at index 0, y at index 1, z at index 2)
@@ -245,77 +181,52 @@ inline void predict(
   T * const vel_angular2,
   T * const acc_linear2)
 {
-  // conversion from array to eigen
-  // TODO: Check how to map Eigen::Quaternion to quaternion as a C array:
-  // Eigen stores component in order x, y, z, w, while in fuse orientation3d 
-  // variable the order is w, x, y, z
-  Eigen::Map<const Eigen::Matrix<T, 3, 1>> position1_map(position1);
-  // Eigen::Map<const Eigen::Quaternion<T>> orientation1_map(orientation1);
-  Eigen::Quaternion<T> orientation1_map;
-  orientation1_map.w() = orientation1[0];
-  orientation1_map.x() = orientation1[1];
-  orientation1_map.y() = orientation1[2];
-  orientation1_map.z() = orientation1[3];
-  Eigen::Map<const Eigen::Matrix<T, 3, 1>> vel_linear1_map(vel_linear1);
-  Eigen::Map<const Eigen::Matrix<T, 3, 1>> vel_angular1_map(vel_angular1);
-  Eigen::Map<const Eigen::Matrix<T, 3, 1>> acc_linear1_map(acc_linear1);
-  
-  // TODO: Check how to map directly output variables to eigen
-  // Using maps fails during compilation probably because Eigen::Map is implicitly converted
-  // to Eigen::Matrix when is passed to predict_eigen
-  Eigen::Matrix<T, 3, 1>  position2_eigen;
-  Eigen::Quaternion<T>    orientation2_eigen;
-  Eigen::Matrix<T, 3, 1>  vel_linear2_eigen;
-  Eigen::Matrix<T, 3, 1>  vel_angular2_eigen;
-  Eigen::Matrix<T, 3, 1>  acc_linear2_eigen;
-
-  predict_eigen(
-    position1_map,
-    orientation1_map,
-    vel_linear1_map,
-    vel_angular1_map,
-    acc_linear1_map,
+  predict(
+    position1[0],
+    position1[1],
+    position1[2],
+    orientation1[0],
+    orientation1[1],
+    orientation1[2],
+    vel_linear1[0],
+    vel_linear1[1],
+    vel_linear1[2],
+    vel_angular1[0],
+    vel_angular1[1],
+    vel_angular1[2],
+    acc_linear1[0],
+    acc_linear1[1],
+    acc_linear1[2],
     dt,
-    position2_eigen,
-    orientation2_eigen,
-    vel_linear2_eigen,
-    vel_angular2_eigen,
-    acc_linear2_eigen
-  );
-
-  // Convert back to C array
-  position2[0] = position2_eigen.x();
-  position2[1] = position2_eigen.y();
-  position2[2] = position2_eigen.z();
-  orientation2[0] = orientation2_eigen.w();
-  orientation2[1] = orientation2_eigen.x();
-  orientation2[2] = orientation2_eigen.y();
-  orientation2[3] = orientation2_eigen.z();
-  vel_linear2[0] = vel_linear2_eigen.x();
-  vel_linear2[1] = vel_linear2_eigen.y();
-  vel_linear2[2] = vel_linear2_eigen.z();
-  vel_angular2[0] = vel_angular2_eigen.x();
-  vel_angular2[1] = vel_angular2_eigen.y();
-  vel_angular2[2] = vel_angular2_eigen.z();
-  acc_linear2[0] = acc_linear2_eigen.x();
-  acc_linear2[1] = acc_linear2_eigen.y();
-  acc_linear2[2] = acc_linear2_eigen.z();  
-  }
+    position2[0],
+    position2[1],
+    position2[2],
+    orientation2[0],
+    orientation2[1],
+    orientation2[2],
+    vel_linear2[0],
+    vel_linear2[1],
+    vel_linear2[2],
+    vel_angular2[0],
+    vel_angular2[1],
+    vel_angular2[2],
+    acc_linear2[0],
+    acc_linear2[1],
+    acc_linear2[2]);
+}
 /**
- * @brief Given a state and time delta, predicts a new state + computes the Jacobians - 
- *        version for predicting new states inside the plugin
- * @param[in] position            - First position
- * @param[in] orientation         - First orientation
- * @param[in] vel_linear          - First linear velocity
- * @param[in] vel_angular         - First angular velocity
- * @param[in] acc_linear          - First linear acceleration
+ * @brief Given a state and time delta, predicts a new state
+ * @param[in] position1    - First position
+ * @param[in] orientation1 - First orientation (quaternion)
+ * @param[in] vel_linear1  - First linear velocity
+ * @param[in] vel_angular1 - First angular velocity
+ * @param[in] acc_linear1  - First linear acceleration
  * @param[in] dt - The time delta across which to predict the state
- * @param[out] position_out       - Second position
- * @param[out] orientation_out    - Second orientation
- * @param[out] vel_linear_out     - Second linear velocity
- * @param[out] vel_angular_out    - Second angular velocity
- * @param[out] acc_linear_out     - Second linear acceleration
- * @param[out] state_tf_jacobian  - Jacobian of the motion model
+ * @param[out] position2    - Second position
+ * @param[out] orientation2 - Second orientation (quaternion)
+ * @param[out] vel_linear2  - Second linear velocity
+ * @param[out] vel_angular2 - Second angular velocity
+ * @param[out] acc_linear2  - Second linear acceleration
  */
 inline void predict(
   const fuse_core::Vector3d & position1,
@@ -336,9 +247,6 @@ inline void predict(
     fuse_core::getPitch(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z()),
     fuse_core::getYaw(orientation1.w(), orientation1.x(), orientation1.y(), orientation1.z())
   );
-  fuse_core::wrapAngle2D(rpy.x());
-  fuse_core::wrapAngle2D(rpy.y());
-  fuse_core::wrapAngle2D(rpy.z());
   // 3D material point projection model which matches the one used by r_l.
   double sr = ceres::sin(rpy.x());
   double cr = ceres::cos(rpy.x());
@@ -348,9 +256,7 @@ inline void predict(
   double cy = ceres::cos(rpy.z());
   double cpi = 1.0 / cp;
 
-  fuse_core::Matrix3d tf_pos, tf_rot, tf_vel, tf_acc;
-  fuse_core::Matrix15d state_tf;
-
+  fuse_core::Matrix3d tf_pos, tf_rot;
   tf_pos(0, 0) = (cy * cp);
   tf_pos(0, 1) = (cy * sp * sr - sy * cr);
   tf_pos(0, 2) = (cy * sp * cr + sy * sr);
@@ -372,7 +278,7 @@ inline void predict(
   tf_rot(2, 2) = cr * cpi;
 
   // Project the state
-  position2 = position1 + tf_pos * vel_linear1 * dt + 0.5 * tf_pos * acc_linear1 * dt * dt;
+  position2 = position1 + tf_pos * vel_linear1 * dt + tf_pos * acc_linear1 * 0.5 * dt * dt;
   rpy = rpy + tf_rot * vel_angular1 * dt;
   vel_linear2 = vel_linear1 + acc_linear1 * dt;
   vel_angular2 = vel_angular1;
@@ -384,9 +290,9 @@ inline void predict(
 
   // Convert back to quaternion
   orientation2 = Eigen::AngleAxisd(rpy.z(), Eigen::Vector3d::UnitZ()) *
-    Eigen::AngleAxisd(rpy.y(), Eigen::Vector3d::UnitY()) *
-    Eigen::AngleAxisd(rpy.x(), Eigen::Vector3d::UnitX());    
-}
+                Eigen::AngleAxisd(rpy.y(), Eigen::Vector3d::UnitY()) *
+                Eigen::AngleAxisd(rpy.x(), Eigen::Vector3d::UnitX());
+  }
 }  // namespace fuse_models
 
 #endif  // FUSE_MODELS__UNICYCLE_3D_PREDICT_HPP_
