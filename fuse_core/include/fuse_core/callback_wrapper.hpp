073feb69171b024f37d8fb719650157daadb3ce4
Henry Moore <henry.moore@picknik.ai>
2024-09-04 22:18:38 +0000

    change to get building with latest rclcpp

--- fuse_core/include/fuse_core/callback_wrapper.hpp
+++ fuse_core/include/fuse_core/callback_wrapper.hpp
@@ -166,7 +166,7 @@ public:
   /**
    * @brief tell the CallbackGroup that this waitable is ready to execute anything
    */
-  bool is_ready(rcl_wait_set_t * wait_set) override;
+  bool is_ready(rcl_wait_set_t const& wait_set) override;
 
 
   /**
@@ -175,11 +175,11 @@ public:
     waitable_ptr = std::make_shared<CallbackWrapper>();
     node->get_node_waitables_interface()->add_waitable(waitable_ptr, (rclcpp::CallbackGroup::SharedPtr) nullptr);
    */
-  void add_to_wait_set(rcl_wait_set_t * wait_set) override;
+  void add_to_wait_set(rcl_wait_set_t & wait_set) override;
 
   std::shared_ptr<void> take_data() override;
 
-  void execute(std::shared_ptr<void> & data) override;
+  void execute(std::shared_ptr<void> const& data) override;
 
   void addCallback(const std::shared_ptr<CallbackWrapperBase> & callback);
 
