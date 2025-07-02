b137f6e52c14e08b742bbfe16929b4c2d95341a5
cli <cli@locusrobotics.com>
2025-07-02 16:29:17 -0400

    add unit test for the fixed lag smoother.

---
+++ fuse_optimizers/test/launch_tests/test_fixed_lag_smoother.cpp
@@ -0,0 +1,140 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2025, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "common.hpp"
+#include <fuse_optimizers/fixed_lag_smoother.hpp>
+#include <fuse_graphs/hash_graph.hpp>
+
+#include <fuse_graphs/hash_graph_params.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>  // NOLINT
+
+
+
+
+class FixedLagSmootherForTest : public fuse_optimizers::FixedLagSmoother
+{
+public:
+  FixedLagSmootherForTest(
+  fuse_core::node_interfaces::NodeInterfaces<ALL_FUSE_CORE_NODE_INTERFACES> interfaces,
+  fuse_core::Graph::UniquePtr graph=nullptr)
+    : fuse_optimizers::FixedLagSmoother(interfaces, std::move(graph)) {}
+
+  using fuse_optimizers::FixedLagSmoother::terminationTypeToDiagnosticStatus;
+};
+
+class TestFixedLagSmoother : public ::testing::Test
+{
+protected:
+  rclcpp::Node::SharedPtr node_;
+  std::shared_ptr<FixedLagSmootherForTest> smoother_;
+
+  static void SetUpTestSuite()
+  {
+    rclcpp::init(0, nullptr);
+  }
+
+  static void TearDownTestSuite()
+  {
+    rclcpp::shutdown();
+  }
+
+  void SetUp() override
+  {
+    node_ = std::make_shared<rclcpp::Node>("fixed_lag_smoother_test");
+    smoother_ = std::make_shared<FixedLagSmootherForTest>(*node_);
+  }
+
+  void TearDown() override
+  {
+    smoother_.reset();
+    node_.reset();
+  }
+};
+
+TEST_F(TestFixedLagSmoother, terminationTypeToDiagnosticStatus)
+{
+  ceres::TerminationType termination_type;
+  std::vector<std::string> diagnostic_warning_status { "NO_CONVERGENCE" };
+  std::vector<std::string> diagnostic_error_status {"FAILURE", "USER_FAILURE"};
+  diagnostic_msgs::msg::DiagnosticStatus diag_msg;
+
+  // NO_CONVERGENCE as WARNING
+  termination_type = ceres::TerminationType::NO_CONVERGENCE;
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::WARN);
+
+  // NO_CONVERGENCE as OK
+  diagnostic_warning_status = {};
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::OK);
+
+  // CONVERGENCE
+  termination_type = ceres::TerminationType::CONVERGENCE;
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::OK);
+
+  // USER_SUCCESS
+  termination_type = ceres::TerminationType::USER_SUCCESS;
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::OK);
+
+  // FAILURE
+  termination_type = ceres::TerminationType::FAILURE;
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::ERROR);
+
+  // USER_FAILURE
+  termination_type = ceres::TerminationType::USER_FAILURE;
+  diag_msg = smoother_->terminationTypeToDiagnosticStatus(termination_type, diagnostic_warning_status,
+                                                          diagnostic_error_status);
+  EXPECT_EQ(diag_msg.level, diagnostic_msgs::msg::DiagnosticStatus::ERROR);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ fuse_optimizers/test/launch_tests/test_fixed_lag_smoother.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+# Copyright 2025 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+import launch_pytest
+from launch_pytest.actions import ReadyToTest
+from launch_pytest.tools import process as process_tools
+
+import pytest
+
+
+@pytest.fixture
+def test_proc():
+    test_root = '.'
+
+    test_path = os.path.join(test_root, 'test_fixed_lag_smoother')
+
+    cmd = [test_path, '--ros-args']
+    return ExecuteProcess(cmd=cmd, shell=True, output='screen', cached_output=True)
+
+
+@launch_pytest.fixture
+def generate_test_description(test_proc):
+    return LaunchDescription([test_proc, ReadyToTest()])
+
+
+@pytest.mark.launch(fixture=generate_test_description)
+async def test_no_failed_gtests(test_proc, launch_context):
+    await process_tools.wait_for_exit(launch_context, test_proc, timeout=30)
+    assert test_proc.return_code == 0, 'GTests failed'
